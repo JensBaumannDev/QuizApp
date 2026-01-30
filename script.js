c8cb7aefef0d180571cc671b4548cf1ab77d2820
JensBaumannDev <baumannjens@outlook.de>
2026-01-30 20:04:14 +0100

    add progress bar (working with js)

--- script.js
+++ script.js
@@ -103,6 +103,12 @@ function showQuestions() {
     document.getElementById("amount-of-right-questions").innerHTML = rightQuestions;
     document.getElementById("hero-img").src = "./assets/trophy.png"
   } else {
+
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById("progress-bar").innerHTML = `${percent}`
+    document.getElementById("progress-bar").style.width = `${percent}%`;
+
     let question = questions[currentQuestion];
 
     document.getElementById("question-number").innerHTML = currentQuestion + 1;
